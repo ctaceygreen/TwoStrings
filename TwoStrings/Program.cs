a2739601268a40030f45ccfad705d8077770ebd4
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 13:24:58 +0000

    Add project files.

---
+++ TwoStrings/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the twoStrings function below.
+    static string twoStrings(string s1, string s2)
+    {
+
+        HashSet<char> s1Chars = new HashSet<char>();
+        foreach(char s1Char in s1)
+        {
+            s1Chars.Add(s1Char);
+        }
+        bool hasSubChar = false;
+        foreach(char s2Char in s2)
+        {
+            if(s1Chars.Contains(s2Char))
+            {
+                hasSubChar = true;
+            }
+        }
+        return hasSubChar ? "YES" : "NO";
+
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
